094f368c628c4b892af551dc784e0c543f71db3c
Tom jing <1798734338@qq.com>
2020-04-12 18:44:06 +0800

    ROOT commit

---
+++ ROOT/src/main/java/cn/net/comsys/ROOT/RootApplication.java
@@ -0,0 +1,13 @@
+package cn.net.comsys.ROOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootApplication.class, args);
+	}
+
+}
